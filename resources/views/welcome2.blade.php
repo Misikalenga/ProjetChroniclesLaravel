c947ba5c2ad70d268569b9aac6bdf9b6fd58cdba
Daniel Misi <misikalenga@gmail.com>
2025-01-25 16:12:25 +0100

    init version laravel

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="app.css">
+
+        <title>serveur discord</title>
+
+
+    </head>
+    <body>
+    <main id="main_background">
+        <header>
+            <span class="header_logo"><a id="nav_link" href=""><img id="nav_logo" src="../../public/img/logo.png" alt="Logo Chronicles"></a></span>
+            <nav>
+                <ul id="nav_list">
+                    <li>
+                        <span class="nav_list_ico">🏠</span>
+                        <a href="#">ACCUEIL</a>
+                    </li>
+                    <li>
+                        <span class="nav_list_ico">🕮</span>
+                        <a href="../../app/views/reglement.php">REGLEMENT</a>
+                    </li>
+                    <li>
+                        <span class="nav_list_ico">📜</span>
+                        <a href="../../app/views/lore.php">LORE</a>
+                    </li>
+                    <li>
+                        <span class="nav_list_ico">📋</span>
+                        <a href="../../app/views/questionnaire.php">QUESTIONNAIRE</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <div>
+                <h3>🎬Immersion</h3>
+                <p>Vivez l'univers de Grand Theft Auto avec des quêtes et évènements interactifs</p>
+            </div>
+            <button><a href="https://discord.gg/jXZqWbufwB" target="_blank">REJOINS NOUS</a></button>
+            <div>
+                <h3>🎭RolePlay</h3>
+                <p>Incarnez des personnages uniques et plongez dans des histoires captivantes</p>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
